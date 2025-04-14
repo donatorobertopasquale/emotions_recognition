d386bfff3580537ab236c445cad604e07f0bdf71
Gabbo-DEV <gabrieletroccoli40@gmail.com>
2025-04-14 16:18:01 +0200

    Initial commit

---
+++ src/test/java/com/eyxpoliba/emotion_recognition/EmotionRecognitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eyxpoliba.emotion_recognition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmotionRecognitionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
