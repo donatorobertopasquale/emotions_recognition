6a8f9e61123523649861f82139aff2e1f4fd6509
Gabriele Conte <gabriele.conte@it.ey.com>
2025-05-27 09:33:29 +0200

    fix backend

--- video_classifier/api.py
+++ video_classifier/api.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
 import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(__file__), "emotion_recognizer"))
@@ -6,6 +7,14 @@ from emotion_recognizer import EmotionRecognizer, EmotionResponse
 
 app = FastAPI()
 emotion_recognizer = EmotionRecognizer()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/api/predict/", response_model=EmotionResponse)
 async def predict_emotion(file: UploadFile = File(...)):
